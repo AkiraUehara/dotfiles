e9d521eb55044ceb27e25dc15aa49ecb857b1d86
Rogerio Akira Alves Uehara <ruehara@atech.com.br>
2025-10-15 17:09:12 -0300

    ✨ (feat): add custom toggle terminal command

---
+++ .config/nvim/lua/core/commands/init.lua
@@ -0,0 +1,1 @@
+require('core.commands.toggle_terminal')
--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,5 +1,5 @@
 require('core.options')
+require('core.commands')
 require('core.keymaps')
 require('core.autocommands')
 require('core.lazy')
-
